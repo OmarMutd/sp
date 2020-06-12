c3ee4e3c9ad34021674bc8c39a4052856ac38083
Omar Muhanna <omarmuhomar@hotmail.com>
2020-06-11 19:19:36 -0500

    Added image and link to google maps for about page of location

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Garland from '../../garland.jpeg'
 
 function About() {
     return (
         <div>
-            <h3>About Page!</h3>
+            <p align="center">
+                < a href=
+                    "https://www.google.com/maps/dir//Soprano's+Cafe+%26+Pizza+Subs,+North+Jupiter+Road,+Garland,+TX/@32.9627729,-96.7172416,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x864c1e947d7f914f:0x2d7d9cc0009dc0bb!2m2!1d-96.6822222!2d32.9627778"
+                    target="_blank">
+                    <img src={Garland} alt='store hours' />
+                </a>
+            </p>
         </div>
     )
 }
