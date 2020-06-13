e69b32def1af1a87c0b2c3a82b87326974064030
Omar Muhanna <omarmuhomar@hotmail.com>
2020-06-13 16:39:15 -0500

    set up icons css for menu page

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import './Menu.css';
 import { Link } from 'react-router-dom';
+import SopranosHours from '../../sopranos.jpeg'
 
 function Menu() {
     return (
         <div>
             <h1 className='Category-title'>Menu</h1>
             <ul className='category-navigation'>
-                <Link to='/Pasta'><li className='menu-txt'>Pasta</li></Link>
-                <Link to='/Subs'><li className='menu-txt'>Subs</li></Link>
-                <Link to='/Gourmet'><li className='menu-txt'>Gourmet</li></Link>
-                <Link to='/SoupsSalads'><li className='menu-txt'>Soups And Salads</li></Link>
-                <Link to='/Wings'><li className='menu-txt'> Wings and Things</li></Link>
-                <Link to='/Kids'><li className='menu-txt'>Kids</li></Link>
-                <Link to='/Desserts'><li className='menu-txt'>Desserts</li></Link>
-                <Link to='/Beverages'><li className='menu-txt'>Beverages</li></Link>
+                <Link to='/Pasta'><li className='menu-txt'><img src={SopranosHours} alt='store hours' className='icon-img' /> Pasta</li></Link>
+                <Link to='/Subs'><li className='menu-txt'><img src={SopranosHours} alt='store hours' className='icon-img' />Subs</li></Link>
+                <Link to='/Gourmet'><li className='menu-txt'><img src={SopranosHours} alt='store hours' className='icon-img' />Gourmet</li></Link>
+                <Link to='/SoupsSalads'><li className='menu-txt'><img src={SopranosHours} alt='store hours' className='icon-img' />Soups And Salads</li></Link>
+                <Link to='/Wings'><li className='menu-txt'><img src={SopranosHours} alt='store hours' className='icon-img' /> Wings and Things</li></Link>
+                <Link to='/Kids'><li className='menu-txt'><img src={SopranosHours} alt='store hours' className='icon-img' />Kids</li></Link>
+                <Link to='/Desserts'><li className='menu-txt'><img src={SopranosHours} alt='store hours' className='icon-img' />Desserts</li></Link>
+                <Link to='/Beverages'><li className='menu-txt'><img src={SopranosHours} alt='store hours' className='icon-img' />Beverages</li></Link>
 
 
             </ul>
